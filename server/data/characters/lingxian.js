a8645fcc1dffa90c15f2e5e722b8ca7390d65a0e
Heaven-Fang <3355895570@qq.com>
2026-03-02 01:00:49 +0800

    123

--- server/data/characters/lingxian.js
+++ server/data/characters/lingxian.js
@@ -11,11 +11,11 @@ module.exports = {
     initSP: 1,
     talent: {
         name: '波长控制',
-        description: '爱丽丝召唤的所有人偶(HP:4，DEF:0)为爱丽丝分摊50%的伤害(包含普通伤害与真实伤害)(若普通伤害为奇数，则人偶分摊较大部分的伤害)'
+        description: 'SP不自然回复，仅有铃仙命中对手时SP+1。相邻回合连续命中，SP额外+1。'
     },
     skills: [
-        { id: 1, name: '人里药郎', type: 'active', timing: 'before', spCost: 0, description: '第一回合开始时召唤一个人偶。回合开始时，场上每存在一个人偶，爱丽丝ATK+8' },
-        { id: 2, name: '生神停止', type: 'active', timing: 'before', spCost: 1, description: '立即召唤1个蓬莱人偶(存在2回合后(包含本回合)在第三个回合开始时自爆并消失，造成1*当前人偶数+2的普通伤害)' },
-        { id: 3, name: '真实之月', type: 'active', timing: 'before', spCost: 2, description: '立即召唤1个上海人偶(若HP未归零则永久存在，每回合开始时对对方造成1*人偶数+1的普通伤害，该人偶判定在蓬莱人偶自爆之前)' }
+        { id: 1, name: '人里药郎', type: 'active', timing: 'before', spCost: 0, description: '降低SP获得HP。' },
+        { id: 2, name: '生神停止', type: 'active', timing: 'before', spCost: 1, description: '在对方使用瞬间技能后，自己可立刻取消对方技能。使用技能瞬间清空自身负面buff。（整局游戏仅可触发2次）' },
+        { id: 3, name: '真实之月', type: 'active', timing: 'before', spCost: 2, description: '消耗自身HP，对对方造成大量真实伤害。' }
     ]
 };
